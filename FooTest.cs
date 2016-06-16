8f4a2fe646f886cc59390f67ad78746a3f524d57
IEUser <IEUser>
2016-06-15 20:55:11 -0700

    Initial commit

---
+++ FooTest.cs
@@ -0,0 +1,15 @@
+﻿namespace NUnit
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FooTest
+    {
+        [Test]
+        public void DoFooShouldReturnFoo()
+        {
+            Foo foo = new Foo();
+            Assert.AreEqual("foo", foo.DoFoo());
+        }
+    }
+}
